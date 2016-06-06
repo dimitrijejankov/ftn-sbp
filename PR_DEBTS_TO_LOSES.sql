89d2dee0944244c44189f9796621f099f815a50c
Dimitrije Jankov <dimitrijejankov@gmail.com>
2016-06-06 05:45:43 +0200

    Fixed some bugs

--- PR_DEBTS_TO_LOSES.sql
+++ PR_DEBTS_TO_LOSES.sql
@@ -7,7 +7,7 @@ DST TG_PARENT_ACCOUNT_PACKAGE.ACCOUNT_ID_T;
 
 BEGIN
   -- Transfer all the entries for the loses (351) of this year to the cumulative loses (350)
-  SELECT a.CA_ID, a.AC_ID 
+  SELECT a.AC_ID, a.CA_ID 
   BULK COLLECT INTO ACCOUNTS 
   FROM "Account" a 
   WHERE a.CA_ID=SRC_CA_ID AND a.CA_ID = '351';
@@ -20,13 +20,16 @@ BEGIN
   END LOOP;
   
   -- Transfer the unpaid captial (00 or 31) to the loses for the current year (351) 
-  SELECT a.CA_ID, a.AC_ID 
+  SELECT a.AC_ID, a.CA_ID 
   BULK COLLECT INTO ACCOUNTS 
   FROM "Account" a WHERE 
-  a.CA_ID=SRC_CA_ID AND (a.CA_ID LIKE '00*' OR a.CA_ID LIKE '31*');
+  a.CA_ID=SRC_CA_ID AND (a.AC_ID LIKE '00%' OR a.AC_ID LIKE '31%');
   
   DST.CA_ID := SRC_CA_ID;
   DST.AC_ID := '351';
 
+  FOR idx in 1..ACCOUNTS.COUNT LOOP
+    PR_REWIRE_JOURNAL_VAUCHER(ACCOUNTS(idx), DST);
+  END LOOP;
   
 END PR_DEBTS_TO_LOSES;
